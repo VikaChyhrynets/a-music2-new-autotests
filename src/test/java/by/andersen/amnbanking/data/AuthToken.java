6cabecdba6f6cfd07fc1990884b90dddf3ce7f2b
Igor Kargapolov <i.kargapolov@andersenlab.com>
2022-05-17 14:59:21 +0500

    Add request for getting authKey Add test for authorization with valid sms code (still 401)

--- src/test/java/by/andersen/amnbanking/data/AuthToken.java
+++ src/test/java/by/andersen/amnbanking/data/AuthToken.java
@@ -2,18 +2,17 @@ package by.andersen.amnbanking.data;
 
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.Header;
-import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
-import static by.andersen.amnbanking.data.DataUrls.*;
+import static by.andersen.amnbanking.data.BaseRequest.reqSpec;
+import static by.andersen.amnbanking.data.DataUrls.API_HOST;
+import static by.andersen.amnbanking.data.DataUrls.API_LOGIN;
 import static by.andersen.amnbanking.data.RequestAndResponseSpec.REQUEST_SPECIFICATION;
 import static io.restassured.RestAssured.given;
 
-public class BaseRequest {
-
-    static RequestSpecification reqSpec = new RequestSpecBuilder()
+public class AuthToken {
+    private static final RequestSpecification reqSpec = new RequestSpecBuilder()
             .addRequestSpecification(REQUEST_SPECIFICATION).build();
-
 
     public static Header getAuthToken(String login, String password) {
         return given()
@@ -26,15 +25,5 @@ public class BaseRequest {
                 .then()
                 .extract()
                 .headers().get("Authorization");
-    }
-
-    public static Response authorizeWithSessionCode(Header authKey) {
-        return given()
-                .spec(reqSpec)
-                .header(authKey)
-                .body("{\"smsCode\": \"1234\"\n}")
-                .post(API_HOST + API_SESSIONCODE)
-                .then()
-                .extract().response();
     }
 }
--- src/test/java/by/andersen/amnbanking/data/RequestAndResponseSpec.java
+++ src/test/java/by/andersen/amnbanking/data/RequestAndResponseSpec.java
@@ -1,15 +1,24 @@
 package by.andersen.amnbanking.data;
 
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 
 import static by.andersen.amnbanking.data.DataUrls.API_URL;
 
-public class RequestSpec {
+public class RequestAndResponseSpec {
     public static final RequestSpecification REQUEST_SPECIFICATION = new RequestSpecBuilder()
             .setBaseUri(API_URL)
             .setContentType(ContentType.JSON)
             .setAccept("*/*")
+            .setRelaxedHTTPSValidation()
+            .build();
+
+    public static final ResponseSpecification RESPONSE_SPECIFICATION = new ResponseSpecBuilder()
+            .expectStatusCode(200)
+            .expectStatusLine("HTTP/1.1 200 ")
+            .expectContentType(ContentType.JSON)
             .build();
 }
