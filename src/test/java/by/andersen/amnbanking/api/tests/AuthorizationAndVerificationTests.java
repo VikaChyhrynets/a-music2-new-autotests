548a77902a4cb38b899c70f3d676a01e2be00e4c
Igor Kargapolov <i.kargapolov@andersenlab.com>
2022-05-18 19:13:52 +0500

    Testcases: C5895560 (BE)Enter 3 characters in the SMS code entry field; C5895561 (BE)Enter 5 characters in the SMS code entry field; C5895562 (BE)Leave input field empty; C5895563 (BE)Enter letters in the input field; C5895564 (BE)Enter symbols in the input field

--- src/test/java/by/andersen/amnbanking/api/tests/AuthorizationAndVerificationTests.java
+++ src/test/java/by/andersen/amnbanking/api/tests/AuthorizationAndVerificationTests.java
@@ -3,11 +3,8 @@ package by.andersen.amnbanking.api.tests;
 import by.andersen.amnbanking.utils.TestRails;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
-import org.testng.Assert;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import static by.andersen.amnbanking.data.AuthToken.getAuthToken;
 import static by.andersen.amnbanking.data.AuthWithToken.authWithSessionCode;
 import static by.andersen.amnbanking.data.RequestAndResponseSpec.*;
 import static org.testng.Assert.assertEquals;
@@ -22,7 +19,25 @@ public class AuthorizationAndVerificationTests extends BaseTest {
     }
 
     @Test
-    @TestRails (id = "C5871538")
+    @TestRails (id = "С5895560")
+    void sendSessionCodeWithThreeDigits() {
+        Response resp = authWithSessionCode("231");
+        assertEquals((resp.getStatusCode()), 400);
+        assertEquals(resp.body().asString(),
+                "{\"message\":\"Sms code contains invalid characters\"}");
+    }
+
+    @Test
+    @TestRails (id = "С5895561")
+    void sendSessionCodeWithFiveDigits() {
+        Response resp = authWithSessionCode("23231");
+        assertEquals((resp.getStatusCode()), 400);
+        assertEquals(resp.body().asString(),
+                "{\"message\":\"Sms code contains invalid characters\"}");
+    }
+
+    @Test
+    @TestRails (id = "C5895562")
     void sendBlankSessionCode() {
         Response resp = authWithSessionCode("");
         assertEquals((resp.getStatusCode()), 400);
@@ -31,11 +46,21 @@ public class AuthorizationAndVerificationTests extends BaseTest {
     }
 
     @Test
-    @TestRails (id = "???")
-    void sendInvalidSessionCode() {
-        Response resp = authWithSessionCode("2311");
-        assertEquals((resp.getStatusCode()), 403);
-//      assertEquals(resp.getBody().);
+    @TestRails (id = "С5895563")
+    void sendSessionCodeWithLetters() {
+        Response resp = authWithSessionCode("brab");
+        assertEquals((resp.getStatusCode()), 400);
+        assertEquals(resp.body().asString(),
+                "{\"message\":\"Sms code contains invalid characters\"}");
+    }
+
+    @Test
+    @TestRails (id = "С5895564")
+    void sendSessionCodeWithSymbols() {
+        Response resp = authWithSessionCode("+++*");
+        assertEquals((resp.getStatusCode()), 400);
+        assertEquals(resp.body().asString(),
+                "{\"message\":\"Sms code contains invalid characters\"}");
     }
 
 }
