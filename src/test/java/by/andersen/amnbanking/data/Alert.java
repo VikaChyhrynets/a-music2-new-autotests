a6e5a04e90844357b1a7a0320fb47bbbeeaef649
Ladauser <sovyhka29@gmail.com>
2022-05-24 09:55:24 +0300

    added negative tests for registration fields, and alert

--- src/test/java/by/andersen/amnbanking/data/Alert.java
+++ src/test/java/by/andersen/amnbanking/data/Alert.java
@@ -2,7 +2,8 @@ package by.andersen.amnbanking.data;
 
 public enum Alert {
     EMPTY_LOGIN_OR_PASSWORD_FIELDS("Field must be filled"),
-    LESS_8_SYMBOL_LOGIN_OR_PASSWORD_FIELDS("Field should contain at least 8 symbols"),
+    LESS_7_SYMBOL_LOGIN_OR_PASSWORD_FIELDS("Field should contain at least 7 symbols"),
+    LOGIN_OR_PASSWORD_FIELDS_MORE_TWENTY_SYMBOLS("Must be 20 characters or less"),
     FORBIDDEN_CHARACTERS_LOGIN_OR_PASSWORD_FIELDS("Field should contain letters, 1 capital letter, 1 number");
     String value;
 
