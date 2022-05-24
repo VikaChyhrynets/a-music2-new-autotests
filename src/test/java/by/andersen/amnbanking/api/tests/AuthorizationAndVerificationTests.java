deadd74023f096b3e2b0b7e524c670906c75f102
Igor Kargapolov <i.kargapolov@andersenlab.com>
2022-05-24 11:07:53 +0500

    Session code test-cases added

--- src/test/java/by/andersen/amnbanking/api/tests/AuthorizationAndVerificationTests.java
+++ src/test/java/by/andersen/amnbanking/api/tests/AuthorizationAndVerificationTests.java
@@ -49,9 +49,25 @@ public class AuthorizationAndVerificationTests extends BaseTest {
     }
 
     @Test()
+    @TestRails (id = "С5895563")
+    void sendSessionCodeWithLetter() {
+        Response resp = new PostAdapters().authWithSessionCode("123a", USER_SESSION_CODE_LOGIN, USER_PASS).as(Response.class);
+        assertEquals(resp.getMessage(),
+                "Sms code contains invalid characters");
+    }
+
+    @Test()
     @TestRails (id = "С5895564")
     void sendSessionCodeWithSymbols() {
         Response resp = new PostAdapters().authWithSessionCode("+++*", USER_SESSION_CODE_LOGIN, USER_PASS).as(Response.class);
+        assertEquals(resp.getMessage(),
+                "Sms code contains invalid characters");
+    }
+
+    @Test()
+    @TestRails (id = "С5895564")
+    void sendSessionCodeWithSymbol() {
+        Response resp = new PostAdapters().authWithSessionCode("123*", USER_SESSION_CODE_LOGIN, USER_PASS).as(Response.class);
         assertEquals(resp.getMessage(),
                 "Sms code contains invalid characters");
     }
