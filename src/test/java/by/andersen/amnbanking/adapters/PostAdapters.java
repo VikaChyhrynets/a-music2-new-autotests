f5f157b5c688a12b7591f3d2a9271694d67e52bd
Igor Kargapolov <i.kargapolov@andersenlab.com>
2022-06-02 23:18:12 +0500

    Added: -Adapter for obtaining cookie from ID checking -Valid passport check -Not registered passport check

--- src/test/java/by/andersen/amnbanking/adapters/PostAdapters.java
+++ src/test/java/by/andersen/amnbanking/adapters/PostAdapters.java
@@ -1,11 +1,15 @@
 package by.andersen.amnbanking.adapters;
 
 import by.andersen.amnbanking.api.tests.BaseTest;
+import io.restassured.http.Cookie;
 import io.restassured.response.Response;
 import io.restassured.response.ResponseBody;
 
 import static by.andersen.amnbanking.data.AuthToken.getAuthToken;
-import static by.andersen.amnbanking.data.DataUrls.*;
+import static by.andersen.amnbanking.data.DataUrls.API_HOST;
+import static by.andersen.amnbanking.data.DataUrls.API_SESSIONCODE;
+import static by.andersen.amnbanking.data.DataUrls.CHANGE_PASSWORD;
+import static by.andersen.amnbanking.data.DataUrls.CHECK_PASSPORT;
 import static by.andersen.amnbanking.data.RequestAndResponseSpec.REQ_SPEC;
 import static io.restassured.RestAssured.given;
 
@@ -49,6 +53,30 @@ public class PostAdapters extends BaseTest {
                 .post(API_HOST + API_SESSIONCODE)
                 .then()
                 .log().all()
+                .extract().response();
+    }
+
+    public static Cookie checkPassportAndGetCookie(String passport) {
+
+        return given()
+                .spec(REQ_SPEC)
+                .body("{\"passport\": \"" + passport + "\"\n}")
+                .post(API_HOST + CHANGE_PASSWORD + CHECK_PASSPORT)
+                .then()
+                .log()
+                .all()
+                .extract().detailedCookie("Login");
+    }
+
+    public static Response checkPassport(String passport) {
+
+        return given()
+                .spec(REQ_SPEC)
+                .body("{\"passport\": \"" + passport + "\"\n}")
+                .post(API_HOST + CHANGE_PASSWORD + CHECK_PASSPORT)
+                .then()
+                .log()
+                .all()
                 .extract().response();
     }
 
