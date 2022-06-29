a3a68864cb3625b40c750c252727ee0243a5206a
Ladauser <sovyhka29@gmail.com>
2022-06-29 12:20:16 +0300

    delete @test

--- src/test/java/by/andersen/amnbanking/DBConnector/DBConnector.java
+++ src/test/java/by/andersen/amnbanking/DBConnector/DBConnector.java
@@ -1,7 +1,6 @@
 package by.andersen.amnbanking.DBConnector;
 
 import by.andersen.amnbanking.utils.BDHelper;
-import org.testng.annotations.Test;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -11,11 +10,8 @@ public class DBConnector {
     DBWorker worker = new DBWorker();
     Statement statement;
     String query;
-    String line;
-   public static String LOGIN_USER;
+    public static String LOGIN_USER;
 
-
-    @Test
     public void deleteUser(String login) throws SQLException {
         query = new BDHelper().setRequestForDeleteUser(login);
         statement = worker.getConnection().createStatement();
@@ -23,7 +19,6 @@ public class DBConnector {
         statement.close();
     }
 
-    @Test
     public void getFirstRegistrationUserFromDB() throws SQLException {
         query = new BDHelper().getUserFormBD();
         statement = worker.getConnection().createStatement();
