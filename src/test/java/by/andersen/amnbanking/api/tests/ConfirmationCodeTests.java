68f7081592460c5e1919a8ad6c93ce9baff447a5
raman-voitka <r.voitka@andersenlab.com>
2022-06-30 20:05:43 +0200

    ConfirmationCodeTests, PasswordRecoveryTests Was Changed

--- src/test/java/by/andersen/amnbanking/api/tests/ConfirmationCodeTests.java
+++ src/test/java/by/andersen/amnbanking/api/tests/ConfirmationCodeTests.java
@@ -7,11 +7,11 @@ import io.qameta.allure.Story;
 import jsonBody.Response;
 import org.testng.annotations.Test;
 
+import java.sql.SQLException;
+
 import static by.andersen.amnbanking.data.AuthToken.getAuthToken;
-import static by.andersen.amnbanking.data.DataUrls.API_HOST;
-import static by.andersen.amnbanking.data.DataUrls.API_SESSIONCODE;
-import static by.andersen.amnbanking.data.DataUrls.LOGIN_WITH_PASSPORT_REG;
-import static by.andersen.amnbanking.data.DataUrls.PASSWORD_WITH_PASSPORT_REG;
+import static by.andersen.amnbanking.data.DataUrls.*;
+import static by.andersen.amnbanking.utils.JsonObjectHelper.setFilterType;
 import static by.andersen.amnbanking.utils.JsonObjectHelper.setSmsCode;
 import static org.testng.Assert.assertEquals;
 
@@ -20,35 +20,35 @@ public class ConfirmationCodeTests extends BaseTest {
 
     @Test(description = "Valid session code")
     @Step("Sending valid code")
-    @TestRails (id = "C5888309")
+    @TestRails(id = "C5888309")
     void sendValidSessionCode() {
         String authToken = getAuthToken(LOGIN_WITH_PASSPORT_REG, PASSWORD_WITH_PASSPORT_REG);
-       Response resp = new PostAdapters().post(setSmsCode("1234"), API_HOST + API_SESSIONCODE, authToken, 200).as(Response.class);
-       assertEquals(resp.getMessage(), "Session code is correct");
+        Response resp = new PostAdapters().post(setSmsCode("1234"), API_HOST + API_SESSIONCODE, authToken, 200).as(Response.class);
+        assertEquals(resp.getMessage(), "Session code is correct");
     }
 
     @Test(description = "Three digit session code")
     @Step("Sending three digits code")
-    @TestRails (id = "С5895560")
+    @TestRails(id = "С5895560")
     void sendSessionCodeWithThreeDigits() {
         String authToken = getAuthToken(LOGIN_WITH_PASSPORT_REG, PASSWORD_WITH_PASSPORT_REG);
         Response resp = new PostAdapters().post(setSmsCode("231"), API_HOST + API_SESSIONCODE, authToken, 400).as(Response.class);
-        assertEquals(resp.getMessage(),"Sms code contains invalid characters");
+        assertEquals(resp.getMessage(), "Sms code contains invalid characters");
     }
 
     @Test(description = "Five digits session code")
     @Step("Sending five digits code")
-    @TestRails (id = "С5895561")
+    @TestRails(id = "С5895561")
     void sendSessionCodeWithFiveDigits() {
         String authToken = getAuthToken(LOGIN_WITH_PASSPORT_REG, PASSWORD_WITH_PASSPORT_REG);
-        Response resp = new PostAdapters().post(setSmsCode("12345"), API_HOST + API_SESSIONCODE, authToken,400 ).as(Response.class);
+        Response resp = new PostAdapters().post(setSmsCode("12345"), API_HOST + API_SESSIONCODE, authToken, 400).as(Response.class);
         assertEquals(resp.getMessage(),
                 "Sms code contains invalid characters");
     }
 
     @Test(description = "Blank session code")
     @Step("Sending blank code")
-    @TestRails (id = "C5895562")
+    @TestRails(id = "C5895562")
     void sendBlankSessionCode() {
         String authToken = getAuthToken(LOGIN_WITH_PASSPORT_REG, PASSWORD_WITH_PASSPORT_REG);
         Response resp = new PostAdapters().post(setSmsCode(""), API_HOST + API_SESSIONCODE, authToken, 400).as(Response.class);
@@ -58,7 +58,7 @@ public class ConfirmationCodeTests extends BaseTest {
 
     @Test(description = "Code with only letters")
     @Step("Sending code with only letters")
-    @TestRails (id = "С5895563")
+    @TestRails(id = "С5895563")
     void sendSessionCodeWithLetters() {
         String authToken = getAuthToken(LOGIN_WITH_PASSPORT_REG, PASSWORD_WITH_PASSPORT_REG);
         Response resp = new PostAdapters().post(setSmsCode("brab"), API_HOST + API_SESSIONCODE, authToken, 400).as(Response.class);
@@ -68,27 +68,27 @@ public class ConfirmationCodeTests extends BaseTest {
 
     @Test(description = "Code with one letter")
     @Step("Sending code with one letter")
-    @TestRails (id = "С5895563")
+    @TestRails(id = "С5895563")
     void sendSessionCodeWithLetter() {
         String authToken = getAuthToken(LOGIN_WITH_PASSPORT_REG, PASSWORD_WITH_PASSPORT_REG);
-        Response resp = new PostAdapters().post(setSmsCode("123a"), API_HOST + API_SESSIONCODE, authToken,400).as(Response.class);
+        Response resp = new PostAdapters().post(setSmsCode("123a"), API_HOST + API_SESSIONCODE, authToken, 400).as(Response.class);
         assertEquals(resp.getMessage(),
                 "Sms code contains invalid characters");
     }
 
     @Test(description = "Code with only symbols")
     @Step("Sending code with only special symbols")
-    @TestRails (id = "С5895564")
+    @TestRails(id = "С5895564")
     void sendSessionCodeWithSymbols() {
         String authToken = getAuthToken(LOGIN_WITH_PASSPORT_REG, PASSWORD_WITH_PASSPORT_REG);
-        Response resp = new PostAdapters().post(setSmsCode("+++*"), API_HOST + API_SESSIONCODE, authToken,400).as(Response.class);
+        Response resp = new PostAdapters().post(setSmsCode("+++*"), API_HOST + API_SESSIONCODE, authToken, 400).as(Response.class);
         assertEquals(resp.getMessage(),
                 "Sms code contains invalid characters");
     }
 
     @Test(description = "Code with one symbol")
     @Step("Sending code with one special symbol")
-    @TestRails (id = "С5895564")
+    @TestRails(id = "С5895564")
     void sendSessionCodeWithSymbol() {
         String authToken = getAuthToken(LOGIN_WITH_PASSPORT_REG, PASSWORD_WITH_PASSPORT_REG);
         Response resp = new PostAdapters().post(setSmsCode("123&"), API_HOST + API_SESSIONCODE, authToken, 400).as(Response.class);
@@ -98,7 +98,7 @@ public class ConfirmationCodeTests extends BaseTest {
 
     @Test(description = "Code with one space")
     @Step("Sending code with one space")
-    @TestRails (id = "С5900178")
+    @TestRails(id = "С5900178")
     void sendSessionCodeWithSpace() {
         String authToken = getAuthToken(LOGIN_WITH_PASSPORT_REG, PASSWORD_WITH_PASSPORT_REG);
         Response resp = new PostAdapters().post(setSmsCode("123 "), API_HOST + API_SESSIONCODE, authToken, 400).as(Response.class);
@@ -108,7 +108,7 @@ public class ConfirmationCodeTests extends BaseTest {
 
     @Test(description = "Code with spaces")
     @Step("Sending code with all spaces")
-    @TestRails (id = "С5900284")
+    @TestRails(id = "С5900284")
     void sendSessionCodeWithSpaces() {
         String authToken = getAuthToken(LOGIN_WITH_PASSPORT_REG, PASSWORD_WITH_PASSPORT_REG);
         Response resp = new PostAdapters().post(setSmsCode("    "), API_HOST + API_SESSIONCODE, authToken, 400).as(Response.class);
@@ -118,7 +118,7 @@ public class ConfirmationCodeTests extends BaseTest {
 
     @Test(description = "Valid code with space")
     @Step("Sending valid code with space")
-    @TestRails (id = "С5900296")
+    @TestRails(id = "С5900296")
     void sendValidSessionCodeWithSpace() {
         String authToken = getAuthToken(LOGIN_WITH_PASSPORT_REG, PASSWORD_WITH_PASSPORT_REG);
         Response resp = new PostAdapters().post(setSmsCode("12 34"), API_HOST + API_SESSIONCODE, authToken, 400).as(Response.class);
@@ -128,11 +128,28 @@ public class ConfirmationCodeTests extends BaseTest {
 
     @Test(description = "Valid code with space in the end")
     @Step("Sending valid code with space in the end")
-    @TestRails (id = "С5900325")
+    @TestRails(id = "С5900325")
     void sendValidSessionCodeEndingWithSpace() {
         String authToken = getAuthToken(LOGIN_WITH_PASSPORT_REG, PASSWORD_WITH_PASSPORT_REG);
         Response resp = new PostAdapters().post(setSmsCode("1234 "), API_HOST + API_SESSIONCODE, authToken, 400).as(Response.class);
         assertEquals(resp.getMessage(),
                 "Sms code contains invalid characters");
+    }
+
+    @Test(description = "Sending a code again to confirm the login when the ban has not expired, negative test")
+    @Step("Sending a code again to confirm the login when the ban has not expired, negative test")
+    @TestRails(id = "C5937934")
+    void sendSMSCodeAgainWhenTheBanHasNotExpired() throws SQLException {
+        createUser();
+        String authToken = getAuthToken("Eminem79", "111Gv5dvvf511");
+        for (int i = 0; i < 3; i++) {
+            new PostAdapters().post(setSmsCode(WRONG_SMS_CODE), API_HOST + API_SESSIONCODE, authToken, 400);
+        }
+        assertEquals(new PostAdapters().post(setSmsCode(WRONG_SMS_CODE), API_HOST + API_SESSIONCODE, authToken, 423).as(Response.class).getMessage(),
+                "Ban time is not over yet...");
+        assertEquals(new PostAdapters().post(setFilterType("SMS_FOR_SESSION"),
+                API_HOST + SMS_CODE, authToken, 423).as(Response.class).getMessage(),
+                "Ban time is not over yet...");
+        deleteUser();
     }
 }
