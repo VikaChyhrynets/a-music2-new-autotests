591a86f333f2686920b7f91ee5b12e11ac2d0798
Ladauser <sovyhka29@gmail.com>
2022-07-01 12:56:33 +0300

    add wait for method confirmationCodeWindowIsOpen

--- src/test/java/by/andersen/amnbanking/ui/pages/ConfirmationCodeModalPage.java
+++ src/test/java/by/andersen/amnbanking/ui/pages/ConfirmationCodeModalPage.java
@@ -1,15 +1,18 @@
 package by.andersen.amnbanking.ui.pages;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.$;
+import static java.time.Duration.ofSeconds;
 
 public class ConfirmationCodeModalPage extends BasePage {
     public SelenideElement confirmationCodeModal = $(By.id(":r5:"));
 
-    public boolean confirmationCodeWindowIsOpen(){
+    public boolean confirmationCodeWindowIsOpen() {
+        this.confirmationCodeModal.shouldBe(Condition.visible, ofSeconds(5));
 
-        return this.confirmationCodeModal.isDisplayed();
+        return true;
     }
 }
