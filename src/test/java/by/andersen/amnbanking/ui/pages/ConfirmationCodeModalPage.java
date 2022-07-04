898794ce10a15a494da72e8a88e76ab4e293eb18
Igor Kargapolov <i.kargapolov@andersenlab.com>
2022-07-04 16:14:24 +0500

    Add test C5931945, C5931947, C5931948

--- src/test/java/by/andersen/amnbanking/ui/pages/ConfirmationCodeModalPage.java
+++ src/test/java/by/andersen/amnbanking/ui/pages/ConfirmationCodeModalPage.java
@@ -1,5 +1,6 @@
 package by.andersen.amnbanking.ui.pages;
 
+import com.codeborne.selenide.ClickOptions;
 import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 import org.openqa.selenium.By;
@@ -8,31 +9,35 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$x;
 
 public class ConfirmationCodeModalPage extends BasePage {
-    public SelenideElement confirmationCodeModal = $(By.id(":r5:"));
-    public SelenideElement smsCodeInput = $(By.id(":r7:"));
-    public SelenideElement confirmCodeButton = $x("//div[contains(@class, 'ModalForm_dialogSubmitButton')]/button");
+    private static final SelenideElement CONFIRMATION_CODE_MODAL = $(By.id(":r5:"));
+    private static final SelenideElement SMS_CODE_INPUT = $(By.id(":r7:"));
+    private static final SelenideElement CONFIRM_CODE_BUTTON = $x("//div[contains(@class, 'ModalForm_dialogSubmitButton')]/button");
     public SelenideElement sendAgainButton = $x("//div[contains(@class, 'ModalBox_dialogSendButton')]/button");
-    public SelenideElement smsCodeValidation = $x("//div[contains(@class, 'Login_formInputSMSMessage')]");
+    private static final SelenideElement SMS_CODE_VALIDATION = $x("//div[contains(@class, 'Login_formInputSMSMessage')]");
+    public static final SelenideElement SUCCESS_LOGIN_MODAL = $(By.id("ModalSuccessfully"));
 
-    public boolean confirmationCodeWindowIsOpen(){
-
-        return this.confirmationCodeModal.isDisplayed();
-    }
+    public boolean confirmationCodeWindowIsOpen() {return CONFIRMATION_CODE_MODAL.isDisplayed();}
 
     @Step("Closing SMS confirmation window by clicking to empty area")
     public LoginPage closeSmsWindowByEmptyClick() {
-        confirmationCodeModal.click(200, 200);
+        CONFIRMATION_CODE_MODAL.click(ClickOptions.usingDefaultMethod().offset(200, 200));
 
         return new LoginPage();
     }
 
     @Step("Entering SMS code")
     public ConfirmationCodeModalPage inputSmsCode(String smsCode) {
-        smsCodeInput.sendKeys(smsCode);
+        SMS_CODE_INPUT.sendKeys(smsCode);
 
         return this;
     }
 
     @Step("Checking validation field")
-    public String checkSmsInputValidationText() {return this.smsCodeValidation.getText();}
+    public String checkSmsInputValidationText() {return SMS_CODE_VALIDATION.getText();}
+
+    @Step("Clicking Confirm button")
+    public LoginPage clickConfirmButton() {
+        CONFIRM_CODE_BUTTON.click();
+        return new LoginPage();
+    }
 }
--- src/test/java/by/andersen/amnbanking/ui/tests/ConfirmationCodeModalTest.java
+++ src/test/java/by/andersen/amnbanking/ui/tests/ConfirmationCodeModalTest.java
@@ -2,14 +2,13 @@ package by.andersen.amnbanking.ui.tests;
 
 import by.andersen.amnbanking.data.Alert;
 import by.andersen.amnbanking.utils.TestRails;
-import com.codeborne.selenide.Condition;
-import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static by.andersen.amnbanking.data.DataUrls.LOGIN_WITH_PASSPORT_REG;
 import static by.andersen.amnbanking.data.DataUrls.PASSWORD_WITH_PASSPORT_REG;
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 public class ConfirmationCodeModalTest extends BaseTest {
 
@@ -20,28 +19,27 @@ public class ConfirmationCodeModalTest extends BaseTest {
 
     @Test(description = "Enter correct 7 symbols in the login and password fields, positive test")
     public void authWithValidDataForLoginAndPasswordFieldsWithSevenSymbols() {
-        loginPage.inputLoginField("Maleficent1")
+        assertTrue(loginPage.inputLoginField("Maleficent1")
                 .inputPasswordField("Number1")
-                .clickLoginButton();
-        confirmationCodeModalPage.confirmationCodeModal.shouldBe(Condition.visible);
+                .clickLoginButton().confirmationCodeWindowIsOpen());
+
     }
 
     @Test(description = "Close sms confirmation window and try to login again, negative test")
     public void closeSmsWindowAndLoginAgain(){
-        loginPage.inputLoginField("UserForTest111")
+        assertTrue(loginPage.inputLoginField("UserForTest111")
                 .inputPasswordField("Password11")
                 .clickLoginButton()
                 .closeSmsWindowByEmptyClick()
-                .clickLoginButton();        ;
-        confirmationCodeModalPage.confirmationCodeModal.shouldBe(Condition.visible);
+                .clickLoginButton().confirmationCodeWindowIsOpen());
     }
 
-    @Test(description = "Authorization with less than 4 characters in the Сode confirmation field, negative test")
+    @Test(description = "Authorization with less than 4 characters in the Code confirmation field, negative test")
     @TestRails(id = "C5931941")
     public void authWithLessThan4Digits() {
         loginPage.loginUserWithCreds(LOGIN_WITH_PASSPORT_REG, PASSWORD_WITH_PASSPORT_REG)
                 .inputSmsCode("123")
-                .smsCodeInput.pressEnter();
+                .clickConfirmButton();
         assertEquals(confirmationCodeModalPage.checkSmsInputValidationText(), Alert.FIELD_SHOULD_CONTAIN_FOUR_NUMBERS.getValue());
     }
 
@@ -50,7 +48,7 @@ public class ConfirmationCodeModalTest extends BaseTest {
     public void authWithLessMore4Digits() {
         loginPage.loginUserWithCreds(LOGIN_WITH_PASSPORT_REG, PASSWORD_WITH_PASSPORT_REG)
                 .inputSmsCode("12345")
-                .smsCodeInput.pressEnter();
+                .clickConfirmButton();
         assertEquals(confirmationCodeModalPage.checkSmsInputValidationText(), Alert.FIELD_SHOULD_CONTAIN_FOUR_NUMBERS.getValue());
     }
 
@@ -59,8 +57,34 @@ public class ConfirmationCodeModalTest extends BaseTest {
     public void authWithEmptySms() {
         loginPage.loginUserWithCreds(LOGIN_WITH_PASSPORT_REG, PASSWORD_WITH_PASSPORT_REG)
                 .inputSmsCode("")
-                .smsCodeInput.pressEnter();
+                .clickConfirmButton();
         assertEquals(confirmationCodeModalPage.checkSmsInputValidationText(), Alert.CONFIRMATION_CODE_MUST_BE_FILLED.getValue());
     }
-    //TODO: неактивные кнопки для смс
+
+    @Test(description = "Authorization with a space at the beginning of the Code confirmation field")
+    @TestRails(id = "C5931945")
+    public void authWithSmsWithSpaceAtTheBeginning() {
+        loginPage.loginUserWithCreds(LOGIN_WITH_PASSPORT_REG, PASSWORD_WITH_PASSPORT_REG)
+                .inputSmsCode(" 1234")
+                .clickConfirmButton();
+        assertEquals(confirmationCodeModalPage.checkSmsInputValidationText(), Alert.FIELD_SHOULD_CONTAIN_FOUR_NUMBERS.getValue());
+    }
+
+    @Test(description = "Authorization with a space at the end of the Code confirmation field")
+    @TestRails(id = "C5931947")
+    public void authWithSmsWithSpaceAtTheEnd() {
+        loginPage.loginUserWithCreds(LOGIN_WITH_PASSPORT_REG, PASSWORD_WITH_PASSPORT_REG)
+                .inputSmsCode("1234 ")
+                .clickConfirmButton();
+        assertEquals(confirmationCodeModalPage.checkSmsInputValidationText(), Alert.FIELD_SHOULD_CONTAIN_FOUR_NUMBERS.getValue());
+    }
+
+    @Test(description = "Authorization with only spaces in the Code confirmation field")
+    @TestRails(id = "C5931948")
+    public void authWithSmsWithOnlySpaces() {
+        loginPage.loginUserWithCreds(LOGIN_WITH_PASSPORT_REG, PASSWORD_WITH_PASSPORT_REG)
+                .inputSmsCode("    ")
+                .clickConfirmButton();
+        assertEquals(confirmationCodeModalPage.checkSmsInputValidationText(), Alert.FIELD_SHOULD_CONTAIN_FOUR_NUMBERS.getValue());
+    }
 }
