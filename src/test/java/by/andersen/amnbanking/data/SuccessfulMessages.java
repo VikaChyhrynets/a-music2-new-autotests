45693e6581fb5b35c2cf351ee6dd46d948d33464
Ladauser <sovyhka29@gmail.com>
2022-07-13 18:19:31 +0300

    added SuccessfulMessages

---
+++ src/test/java/by/andersen/amnbanking/data/SuccessfulMessages.java
@@ -0,0 +1,11 @@
+package by.andersen.amnbanking.data;
+
+public class SuccessfulMessages {
+    public static final String SMS_SENT_SUCCESSFULLY ="Sms sent successfully";
+    public static final String  PASSPORT_IS_VALID="Passport is valid. Sms sent successfully";
+    public static final String  CHANGE_PASSWORD_CODE_CORRECT = "Change password code is correct";
+    public static final String LOGIN_SUCCESS="Access granted";
+    public static final String REGISTRATION_SUCCESS_USER = "User with login: Eminem79 added";
+    public static final String SUCCESSFUL_PASSWORD_CHANGED = "Password changed successfully! Please login again";
+    public static final String SESSION_CODE_CORRECT = "Session code is correct";
+}
