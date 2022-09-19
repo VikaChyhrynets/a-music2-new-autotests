5b1d14607504420fda9897ee4be4d4f3f69c0172
VikaChigrinets <vika.dolgikh@mail.ru>
2022-09-19 14:22:39 +0200

    A2N-832 Tests update__2

--- src/test/java/by/andersen/amnbanking/tests/ui_tests/page/ConfirmationCodeModalPage.java
+++ src/test/java/by/andersen/amnbanking/tests/ui_tests/page/ConfirmationCodeModalPage.java
@@ -14,6 +14,7 @@ public class ConfirmationCodeModalPage extends BasePage {
     private static final By smsCodeField = (By.id(":r7:"));
     private static final By confirmButton = (By.xpath("//*[text()= 'Confirm']"));
     private static final By errorMessageWrongSmsCode = (By.xpath("//div[contains(@class,\"ModalForm_formInputSMS\") and not (.//*[contains(@class,\"MuiFormControl-root\")])]/div"));
+    // TODO 18-20 lines, rewriting. New task.
     private static final By wrongEnteredMessage1Time = (By.xpath("//div[text()= 'Wrong code, 2 attempts left']"));
     private static final By wrongEnteredMessage2Times = (By.xpath("//div[text()= 'Wrong code, 1 attempts left']"));
     private static final By wrongEnteredMessage3Times = (By.xpath("//div[text()= 'You have entered an incorrect SMS code three times, you can try to log in again in 30 minutes']"));
